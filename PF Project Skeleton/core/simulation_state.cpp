aab5a357c01890a937f3f7a825ce7f549d453764
Shahmir Zubair <shahmirzubair63@gmail.com>
2025-11-26 01:08:20 +0500

    All inputs read in simulation_state.cpp

--- PF Project Skeleton/core/simulation_state.cpp
+++ PF Project Skeleton/core/simulation_state.cpp
@@ -24,7 +24,12 @@ int TRAIN_COUNT = 0;
 // SWITCHES
 // ----------------------------------------------------------------------------
 int SWITCH_COUNT = 0;
-int** SWITCHES = NULL;
+char* LETTER = NULL;
+string* MODE = NULL;
+int* INIT = NULL;
+int** K_VALUES = NULL;
+string* STATE0 = NULL;
+string* STATE1 = NULL;
 // ----------------------------------------------------------------------------
 // SPAWN AND DESTINATION POINTS
 // ----------------------------------------------------------------------------
@@ -65,9 +70,16 @@ void allocateSwitchesTrains()
     {
         TRAINS[i] = new int[5]; // tick, x, y, dir, color
     }
-    SWITCHES = new int*[SWITCH_COUNT];
-    for(int i = 0; i < SWITCH_COUNT; i++)
-        SWITCHES[i] = new int[9];
+    LETTER = new char[SWITCH_COUNT];
+    MODE = new string[SWITCH_COUNT];
+    INIT = new int[SWITCH_COUNT];
+    K_VALUES = new int*[SWITCH_COUNT];
+    for(int i = 0; i < SWITCH_COUNT; i++) 
+    {
+        K_VALUES[i] = new int[4]; // up, right, down, left
+    }
+    STATE0 = new string[SWITCH_COUNT];
+    STATE1 = new string[SWITCH_COUNT];
 }
 
 
@@ -150,39 +162,59 @@ int initializeSimulationState()
     ifstream file2(path + file_name);
 
     // fast-forward to SWITCHES:
-    while (getline(file2, line)) {
+    while (getline(file2, line)) 
+    {
         if (line == "SWITCHES:") break;
     }
+for (int i = 0; i < SWITCH_COUNT; i++) 
+{
+    getline(file2, line);   // read one switch line
 
+    // --- Split line into 9 tokens manually ---
+    string tokens[9];
+    int wordCount = 0;
     string current = "";
-    for (int i = 0; i < line.size(); i++) 
+
+    for (int j = 0; j < (int)line.size(); j++) 
     {
-        char c = line[i];
-        if (c == ' ' || c == '\t') 
+        char c = line[j];
+        if (c == ' ') 
         {
             if (current != "") 
             {
-                SWITCHES[wordCount++] = current;
-                current.clear();
+                tokens[wordCount++] = current;
+                current = "";
             }
         } 
         else 
-        {
             current += c;
-        }
     }
-    if (current != "") {
-        SWITCHES[wordCount++] = current; // add last word
+    if (current != "" && wordCount < 9) 
+        tokens[wordCount++] = current;
+
+    LETTER[i] = tokens[0][0];                        // Store Switch
+    MODE[i] = tokens[1];                           // "PER_DIR" or "GLOBAL"
+    INIT[i] = stoi(tokens[2]);                     // initial state (0 or 1)
+
+    K_VALUES[i][0] = stoi(tokens[3]);                  // K_UP
+    K_VALUES[i][1] = stoi(tokens[4]);                  // K_RIGHT
+    K_VALUES[i][2] = stoi(tokens[5]);                  // K_DOWN
+    K_VALUES[i][3] = stoi(tokens[6]);                  // K_LEFT
+
+    STATE0[i]   = tokens[7];                           // label for State0
+    STATE1[i]   = tokens[8];                           // label for State1
     }
 
     // fast-forward to TRAINS:
-    while (getline(file2, line)) {
+    while (getline(file2, line)) 
+    {
         if (line == "TRAINS:") break;
     }
 
     // Read actual train lines
     int idx = 0;
-    while (getline(file2, line)) {
+    while (getline(file2, line)) 
+    {
         if (line.length() == 0) continue;
 
         for(int i=0; i<=8; i++)
@@ -199,20 +231,26 @@ int initializeSimulationState()
 int main()
 {
     initializeSimulationState();
-    // cout<<LVL_NAME<<endl;
-    // cout<<ROWS<<endl;
-    // cout<<COLS<<endl;
-    // cout<<SEED<<endl;
-    // cout<<WEATHER<<endl;
-    // cout<<SWITCH_COUNT<<endl;
-    // cout<<TRAIN_COUNT<<endl;
+    cout<<LVL_NAME<<endl;
+    cout<<ROWS<<endl;
+    cout<<COLS<<endl;
+    cout<<SEED<<endl;
+    cout<<WEATHER<<endl;
+    for(int i=0; i<ROWS; i++)
+    {
+        for(int j=0; j<COLS; j++)
+            cout<<GRID[i][j];
+        cout<<endl;
+    }
     for(int i=0; i<TRAIN_COUNT; i++)
     {
         for(int j=0; j<5; j++)
             cout<<TRAINS[i][j]<<" ";
         cout<<endl;
     }
-    delete[] TRAINS;
-    delete[] SWITCHES;
+    for(int i=0; i < SWITCH_COUNT; i++)
+    {
+        cout<<LETTER[i]<<" "<<MODE[i]<<" "<<INIT[i]<<" "<<K_VALUES[i][0]<<" "<<K_VALUES[i][1]<<" "<<K_VALUES[i][2]<<" "<<K_VALUES[i][3]<<" "<<STATE0[i]<<" "<<STATE1[i]<<endl;
+    }
 
 }
